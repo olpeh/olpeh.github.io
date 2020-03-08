2fc9cee7e0aac757817981739878649d4f9ed146
Olavi Haapala <olavi.haapala@futurice.com>
2020-03-08 21:25:15 +0200

    Change the author to Olavi

--- src/Data/Author.elm
+++ src/Data/Author.elm
@@ -17,9 +17,9 @@ type alias Author =
 
 all : List Author
 all =
-    [ { name = "Dillon Kearns"
-      , avatar = Pages.images.author.dillon
-      , bio = "Elm developer and educator. Founder of Incremental Elm Consulting."
+    [ { name = "Olavi Haapala"
+      , avatar = Pages.images.author.olavi
+      , bio = "Web software developer with a passion for web performance and accessibility."
       }
     ]
 
