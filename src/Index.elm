490b534cf1bda3b1fe5324249af47ed5be360eda
Olavi Haapala <olavi.haapala@futurice.com>
2020-03-15 16:48:29 +0200

    Improve blog index card layout

--- src/Index.elm
+++ src/Index.elm
@@ -6,6 +6,7 @@ import Date
 import Html.Attributes as Attr exposing (class, href)
 import Metadata exposing (Metadata)
 import Pages
+import Pages.ImagePath as ImagePath exposing (ImagePath)
 import Pages.PagePath as PagePath exposing (PagePath)
 import Pages.Platform exposing (Page)
 
@@ -72,21 +73,39 @@ articleIndex metadata =
         [ postPreview metadata ]
 
 
-readMoreLink =
-    text "Continue reading >>"
-
-
 postPreview : Metadata.ArticleMetadata -> Html msg
 postPreview post =
     div
         [ class "p-8" ]
-        [ title post.title
-        , div [ class "mb-8" ]
-            [ div [ class "text-xl" ] [ text post.description ]
-            , Html.hr [ class "my-4" ] []
-            , text (post.published |> Date.format "MMMM ddd, yyyy")
-            , text " by "
-            , text post.author.name
+        [ div [ class "md:flex md:justify-start" ]
+            [ div [ class "flex self-center md:flex-1" ]
+                [ postImageView post.image post.altText
+                ]
+            , div [ class "mt-8 md:ml-8 md:mt-0 md:flex-3" ]
+                [ title post.title
+                , div [ class "mb-8" ]
+                    [ div [ class "text-xl" ] [ text post.description ]
+                    , Html.hr [ class "my-4" ] []
+                    , text (post.published |> Date.format "MMMM ddd, yyyy")
+                    , text " by "
+                    , text post.author.name
+                    , imageCreditsView post.credits
+                    ]
+                ]
             ]
-        , readMoreLink
         ]
+
+
+postImageView : ImagePath Pages.PathKey -> String -> Html msg
+postImageView articleImage altText =
+    img altText [ Attr.src (ImagePath.toString articleImage), Attr.width 200, Attr.height 200, class "rounded-full layered-box-shadow" ]
+
+
+imageCreditsView : Maybe String -> Html msg
+imageCreditsView credits =
+    case credits of
+        Just str ->
+            Html.text (" | " ++ str)
+
+        Nothing ->
+            Html.text ""
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
         secondary: '#ffffff',
         tertiary: '#cccccc',
         footer: '#fafafa'
+      },
+      flex: {
+        '1': '1 1 0%',
+        auto: '1 1 auto',
+        inherit: 'inherit',
+        none: 'none',
+        '2': '2 2 0%',
+        '3': '3 3 0%'
       }
     }
   },
