96c7e0555c619e3b419864c18b81e4feb0f4ec09
Olavi Haapala <olavi.haapala@futurice.com>
2021-02-21 01:11:46 +0200

    Upgrade quicklink to v.2.1.0

--- js/quicklink.js
+++ js/quicklink.js
@@ -1,1 +1,142 @@
-!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n():"function"==typeof define&&define.amd?define(n):e.quicklink=n()}(this,function(){var e={};function n(e){return new Promise(function(n,t){var r=new XMLHttpRequest;r.open("GET",e,r.withCredentials=!0),r.onload=function(){200===r.status?n():t()},r.send()})}var t,r,i=(t="prefetch",((r=document.createElement("link")).relList||{}).supports&&r.relList.supports(t)?function(e){return new Promise(function(n,t){var r=document.createElement("link");r.rel="prefetch",r.href=e,r.onload=n,r.onerror=t,document.head.appendChild(r)})}:n);function o(t,r,o){if(!(e[t]||(o=navigator.connection)&&((o.effectiveType||"").includes("2g")||o.saveData)))return(r?function(e){return null==self.fetch?n(e):fetch(e,{credentials:"include"})}:i)(t).then(function(){e[t]=!0})}var u=u||function(e){var n=Date.now();return setTimeout(function(){e({didTimeout:!1,timeRemaining:function(){return Math.max(0,50-(Date.now()-n))}})},1)},c=new Set,f=new IntersectionObserver(function(e){e.forEach(function(e){if(e.isIntersecting){var n=e.target.href;c.has(n)&&a(n)}})});function a(e){c.delete(e),o(new URL(e,location.href).toString(),f.priority)}return function(e){e=Object.assign({timeout:2e3,priority:!1,timeoutFn:u,el:document},e),f.priority=e.priority;var n=e.origins||[location.hostname],t=e.ignores||[];e.timeoutFn(function(){e.urls?e.urls.forEach(a):Array.from(e.el.querySelectorAll("a"),function(e){f.observe(e),n.length&&!n.includes(e.hostname)||function e(n,t){return Array.isArray(t)?t.some(function(t){return e(n,t)}):(t.test||t).call(t,n.href,n)}(e,t)||c.add(e.href)})},{timeout:e.timeout})}});
+!(function (e, n) {
+  'object' == typeof exports && 'undefined' != typeof module
+    ? n(exports)
+    : 'function' == typeof define && define.amd
+    ? define(['exports'], n)
+    : n(((e = e || self).quicklink = {}));
+})(this, function (e) {
+  function n(e) {
+    return new Promise(function (n, t, r) {
+      (r = new XMLHttpRequest()).open('GET', e, (r.withCredentials = !0)),
+        (r.onload = function () {
+          200 === r.status ? n() : t();
+        }),
+        r.send();
+    });
+  }
+  function t(e) {
+    return window.fetch ? fetch(e, { credentials: 'include' }) : n(e);
+  }
+  var r,
+    o =
+      (r = document.createElement('link')).relList &&
+      r.relList.supports &&
+      r.relList.supports('prefetch')
+        ? function (e) {
+            return new Promise(function (n, t, r) {
+              ((r = document.createElement('link')).rel = 'prefetch'),
+                (r.href = e),
+                (r.onload = n),
+                (r.onerror = t),
+                document.head.appendChild(r);
+            });
+          }
+        : n,
+    i =
+      window.requestIdleCallback ||
+      function (e) {
+        var n = Date.now();
+        return setTimeout(function () {
+          e({
+            didTimeout: !1,
+            timeRemaining: function () {
+              return Math.max(0, 50 - (Date.now() - n));
+            },
+          });
+        }, 1);
+      },
+    c = new Set();
+  function f(e, n, r) {
+    if ((r = navigator.connection)) {
+      if (r.saveData)
+        return Promise.reject(
+          new Error('Cannot prefetch, Save-Data is enabled')
+        );
+      if (/2g/.test(r.effectiveType))
+        return Promise.reject(
+          new Error('Cannot prefetch, network conditions are poor')
+        );
+    }
+    return Promise.all(
+      [].concat(e).map(function (e) {
+        if (!c.has(e))
+          return c.add(e), (n ? t : o)(new URL(e, location.href).toString());
+      })
+    );
+  }
+  (e.listen = function (e) {
+    if ((e || (e = {}), window.IntersectionObserver)) {
+      var n = (function (e) {
+          e = e || 1;
+          var n = [],
+            t = 0;
+          function r() {
+            t < e && n.length > 0 && (n.shift()(), t++);
+          }
+          return [
+            function (e) {
+              n.push(e) > 1 || r();
+            },
+            function () {
+              t--, r();
+            },
+          ];
+        })(e.throttle || 1 / 0),
+        t = n[0],
+        r = n[1],
+        o = e.limit || 1 / 0,
+        u = e.origins || [location.hostname],
+        s = e.ignores || [],
+        a = e.delay || 0,
+        l = [],
+        d = e.timeoutFn || i,
+        h = 'function' == typeof e.hrefFn && e.hrefFn,
+        m = new IntersectionObserver(function (n) {
+          n.forEach(function (n) {
+            if (n.isIntersecting)
+              l.push((n = n.target).href),
+                (function (e, n) {
+                  n ? setTimeout(e, n) : e();
+                })(function () {
+                  -1 !== l.indexOf(n.href) &&
+                    (m.unobserve(n),
+                    c.size < o &&
+                      t(function () {
+                        f(h ? h(n) : n.href, e.priority)
+                          .then(r)
+                          .catch(function (n) {
+                            r(), e.onError && e.onError(n);
+                          });
+                      }));
+                }, a);
+            else {
+              var i = l.indexOf((n = n.target).href);
+              i > -1 && l.splice(i);
+            }
+          });
+        });
+      return (
+        d(
+          function () {
+            (e.el || document).querySelectorAll('a').forEach(function (e) {
+              (u.length && !u.includes(e.hostname)) ||
+                (function e(n, t) {
+                  return Array.isArray(t)
+                    ? t.some(function (t) {
+                        return e(n, t);
+                      })
+                    : (t.test || t).call(t, n.href, n);
+                })(e, s) ||
+                m.observe(e);
+            });
+          },
+          { timeout: e.timeout || 2e3 }
+        ),
+        function () {
+          c.clear(), m.disconnect();
+        }
+      );
+    }
+  }),
+    (e.prefetch = f);
+});
